f9f3372db245a6385681cb927013da133f3a20c5
florianb <flbodevtools@gmail.com>
2026-09-07 23:08:52 +0200

    - add additional project permissions to member types

--- backend/src/main/java/com/backend/project_member/domain/MemberRole.java
+++ backend/src/main/java/com/backend/project_member/domain/MemberRole.java
@@ -9,42 +9,83 @@ public enum MemberRole {
             Set.of(
                     PROJECT_UPDATE,
                     PROJECT_DELETE,
+                    PROJECT_VIEW,
 
                     BOARD_CREATE,
                     BOARD_UPDATE,
                     BOARD_DELETE,
+                    BOARD_VIEW,
 
                     MEMBER_ADD,
                     MEMBER_REMOVE,
                     MEMBER_UPDATE,
+                    MEMBER_VIEW,
 
                     COLUMN_CREATE,
                     COLUMN_DELETE,
-                    COLUMN_UPDATE
+                    COLUMN_UPDATE,
+                    COLUMN_VIEW,
+
+                    CARD_CREATE,
+                    CARD_DELETE,
+                    CARD_UPDATE,
+                    CARD_VIEW
             )
     ),
     ADMIN(
             Set.of(
                     PROJECT_UPDATE,
+                    PROJECT_VIEW,
 
                     BOARD_CREATE,
                     BOARD_UPDATE,
                     BOARD_DELETE,
+                    BOARD_VIEW,
 
                     MEMBER_ADD,
                     MEMBER_REMOVE,
                     MEMBER_UPDATE,
+                    MEMBER_VIEW,
 
                     COLUMN_CREATE,
                     COLUMN_DELETE,
-                    COLUMN_UPDATE
+                    COLUMN_UPDATE,
+                    COLUMN_VIEW,
+
+                    CARD_CREATE,
+                    CARD_DELETE,
+                    CARD_UPDATE,
+                    CARD_VIEW
             )
     ),
     MEMBER(
-            Set.of()
+            Set.of(
+                    PROJECT_VIEW,
+
+                    BOARD_VIEW,
+
+                    MEMBER_VIEW,
+
+                    COLUMN_VIEW,
+
+                    CARD_CREATE,
+                    CARD_DELETE,
+                    CARD_UPDATE,
+                    CARD_VIEW
+            )
     ),
     VIEWER(
-            Set.of()
+            Set.of(
+                    PROJECT_VIEW,
+
+                    BOARD_VIEW,
+
+                    MEMBER_VIEW,
+
+                    COLUMN_VIEW,
+
+                    CARD_VIEW
+            )
     );
 
     private final Set<ProjectPermission> permissions;
